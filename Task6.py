f7514ed7663a920ed4136d9a2b1bb73c3390ee27
BecauseOfCourse <forwork872@gmail.com>
2025-04-30 00:35:53 +0300

    Добавлено шестое задание из модуля

---
+++ Task6.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f"Node({self.value})"
+
+
+class Graph:
+    def __init__(self, root, visited = []):
+        self._root = root
+        self.visited = visited
+
+    def dfs(self):
+        if self._root not in self.visited:
+            print(self._root, end=" ")
+            self.visited.append(self._root)
+            for neighbor in self._root.outbound:
+                t = Graph(neighbor)
+                t.dfs()
+
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+i = Node('i')
+k = Node('k')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+a.point_to(e)
+e.point_to(f)
+e.point_to(g)
+f.point_to(i)
+f.point_to(h)
+g.point_to(k)
+
+g = Graph(a)
+
+print(g.dfs())
